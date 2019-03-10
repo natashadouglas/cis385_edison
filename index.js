dc89cb8518cf98b93d28fe5d31514f247cea1703
ndouglas <44378635+natashadouglas@users.noreply.github.com>
2019-03-10 14:50:16 -0500

    updated index.js and index.ejs
    added code to evaluate high bid and return winning bid confirmation

--- index.js
+++ index.js
@@ -32,9 +32,35 @@ app.post('/', function (req, res) {
 // modified from Scaling an Express.js Application with Memcache on Heroku
 app.post('/bid1', function(req, res){
    console.log(req.body);
-   const bidText = "Place another bid on Social Contribution";
-   const URL="https://socialcontribution.herokuapp.com/";
-   res.send("Bid received, thank you! " + bidText.link(URL));
+    // connect to database
+    const client = pool.connect();
+
+// create parameterized query, insert row into database with indexing of form values
+    const text = 'INSERT INTO bids(item, email, bid) VALUES("item1", $1, $2)';
+
+// generate array from form values; run insert query
+    const values = [req.body.email1, req.body.bid1];
+    client.query(text, values );
+
+// count the number of bids for item
+   const response = client.query ('SELECT COUNT (*) FROM bids WHERE item = "item1"');
+   const itemCount = response.rows[0];
+
+   // if the bid count is greater than or equal to three, declare auction winner; query winner
+    if (itemCount >= 3 ) {
+        const response = client.query ('SELECT * FROM bids WHERE item = "item1" AND bid = (SELECT MAX (bid) FROM bids)');
+        const highBid1 = response.rows[0].bid;
+        const winningEmail1 = response.rows[0].email;
+
+        // display winning bid and associated email address on index.ejs
+        res.render ('index', {highBid1:highBid1, winningEmail1:winningEmail1});
+    } else {
+        const bidText = "Place another bid on Social Contribution";
+        const URL="https://socialcontribution.herokuapp.com/";
+        res.send("Bid received, thank you! " + bidText.link(URL));
+
+    }
+
 });
 app.post('/bid2', function(req, res){
     console.log(req.body);

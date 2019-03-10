eeb9d42896300711fe539d5b6ba2c71eeba8332c
ndouglas <44378635+natashadouglas@users.noreply.github.com>
2019-03-10 18:03:12 -0500

    overhauled index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 // connect utilizing generated pool
 //const { Pool } = require('pg');
 //const pool = new Pool({
-    //connectionString: process.env.DATABASE_URL,
-    //ssl: true
+//connectionString: process.env.DATABASE_URL,
+//ssl: true
 //});
 const pg = require('pg');
 const express = require('express');
@@ -17,29 +17,33 @@ app.set('port', (process.env.PORT || 5000))
 app.set('views', __dirname + '/views')
 app.use(express.static(__dirname + '/public'))
 app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended: true})); 
-app.use(express.json()); 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 // get index.ejs page from server, rendering index
 app.get('/', function (req, res) {
-  res.render('index')
+    res.render('index')
 });
 
 // run app
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+    console.log("Node app is running at localhost:" + app.get('port'))
 });
 
 // render index.ejs on post request
 app.post('/', function (req, res) {
-  res.render('index')
+    res.render('index')
 })
 
 // modified from Scaling an Express.js Application with Memcache on Heroku
 app.post('/bid1', function(req, res){
-   console.log(req.body);
+    console.log(req.body);
     // connect to database
-    const client = pool.connect();
+   // const client = pool.connect();
+    pool.connect(function (err,client,done) {
+        if (err) { console.log("Cannot connect to the DB" + err); }
+
+
 
 // create parameterized query, insert row into database with indexing of form values
     const text = 'INSERT INTO bids(item, email, bid) VALUES("item1", $1, $2)';
@@ -48,13 +52,14 @@ app.post('/bid1', function(req, res){
 // callback
     const values = [req.body.email1, req.body.bid1];
     client.query(text, values, function (err, res) {
+        done();
         if (err) {
             console.log(err.stack)
         } else {
             console.log(res.rows[0])
         }
     });
-
+});
 // count the number of bids for item
     client.query ('SELECT COUNT (*) FROM bids WHERE item = "item1"', function (err,res) {
         if (err) {
@@ -64,18 +69,18 @@ app.post('/bid1', function(req, res){
             const response = res
         }
     });
-   const itemCount = response.rows[0];
+    const itemCount = response.rows[0];
 
-   // if the bid count is greater than or equal to three, declare auction winner; query winner
+    // if the bid count is greater than or equal to three, declare auction winner; query winner
     if (itemCount >= 3 ) {
-       client.query ('SELECT * FROM bids WHERE item = "item1" AND bid = (SELECT MAX (bid) FROM bids)', function (err,res) {
-           if (err) {
-               console.log(err.stack)
-           } else {
-               console.log(res.rows[0])
-               const response = res
-           }
-       });
+        client.query ('SELECT * FROM bids WHERE item = "item1" AND bid = (SELECT MAX (bid) FROM bids)', function (err,res) {
+            if (err) {
+                console.log(err.stack)
+            } else {
+                console.log(res.rows[0])
+                const response = res
+            }
+        });
         const highBid1 = response.rows[0].bid;
         const winningEmail1 = response.rows[0].email;
 
@@ -119,4 +124,3 @@ app.post('/bid6', function(req, res){
     const URL="https://socialcontribution.herokuapp.com/";
     res.send("Bid received, thank you! " + bidText.link(URL));
 });
-

8f32e671e1074f0e0ac8afeb265784450c1ce0c2
ndouglas <44378635+natashadouglas@users.noreply.github.com>
2019-03-11 00:13:40 -0500

    updated image files

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 //connectionString: process.env.DATABASE_URL,
 //ssl: true
 //});
+app.use( express.static( "public" ) );
 const pg = require('pg');
 const express = require('express');
 const app = express();

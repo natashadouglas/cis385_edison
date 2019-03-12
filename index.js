087d7761fbd4636c452e9f38439b4e8e7f7d3c4d
natashadouglas <44378635+natashadouglas@users.noreply.github.com>
2019-03-12 17:20:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -90,32 +90,8 @@ app.post('/bid1', async (req, returns) => {
         });
     });
 
-    const data1 = await pgClient.query("SELECT * FROM bids WHERE item = 'item1' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item1' ) ORDER BY id");
-    const highBid1 = data1.rows[0].bid;
-    const winningEmail1 = data1.rows[0].email;
-
-    const data2 = await pgClient.query("SELECT * FROM bids WHERE item = 'item2' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item2' ) ORDER BY id");
-    const highBid2 = data2.rows[0].bid;
-    const winningEmail2 = data2.rows[0].email;
-
-    const data3 = await pgClient.query("SELECT * FROM bids WHERE item = 'item3' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item3' ) ORDER BY id");
-    const highBid3 = data3.rows[0].bid;
-    const winningEmail3 = data3.rows[0].email;
-
-    const data4 = await pgClient.query("SELECT * FROM bids WHERE item = 'item4' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item4' ) ORDER BY id");
-    const highBid4 = data4.rows[0].bid;
-    const winningEmail4 = data4.rows[0].email;
-
-    const data5 = await pgClient.query("SELECT * FROM bids WHERE item = 'item5' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item5' ) ORDER BY id");
-    const highBid5 = data5.rows[0].bid;
-    const winningEmail5 = data5.rows[0].email;
-
-    const data6 = await pgClient.query("SELECT * FROM bids WHERE item = 'item6' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item6' ) ORDER BY id");
-    const highBid6 = data6.rows[0].bid;
-    const winningEmail6 = data6.rows[0].email;
     
     return returns.redirect('/');
-    //return returns.render('index', {highBid1:highBid1, winningEmail1:winningEmail1, highBid2:highBid2, winningEmail2:winningEmail2, highBid3:highBid3, winningEmail3:winningEmail3, highBid4:highBid4, winningEmail4:winningEmail4, highBid5:highBid5, winningEmail5:winningEmail5, highBid6:highBid6, winningEmail6:winningEmail6});
 });
 
 // modified from Scaling an Express.js Application with Memcache on Heroku
@@ -142,32 +118,8 @@ app.post('/bid2', async (req, returns) => {
         });
     });
 
-    const data1 = await pgClient.query("SELECT * FROM bids WHERE item = 'item1' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item1' ) ORDER BY id");
-    const highBid1 = data1.rows[0].bid;
-    const winningEmail1 = data1.rows[0].email;
-
-    const data2 = await pgClient.query("SELECT * FROM bids WHERE item = 'item2' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item2' ) ORDER BY id");
-    const highBid2 = data2.rows[0].bid;
-    const winningEmail2 = data2.rows[0].email;
-
-    const data3 = await pgClient.query("SELECT * FROM bids WHERE item = 'item3' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item3' ) ORDER BY id");
-    const highBid3 = data3.rows[0].bid;
-    const winningEmail3 = data3.rows[0].email;
-
-    const data4 = await pgClient.query("SELECT * FROM bids WHERE item = 'item4' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item4' ) ORDER BY id");
-    const highBid4 = data4.rows[0].bid;
-    const winningEmail4 = data4.rows[0].email;
-
-    const data5 = await pgClient.query("SELECT * FROM bids WHERE item = 'item5' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item5' ) ORDER BY id");
-    const highBid5 = data5.rows[0].bid;
-    const winningEmail5 = data5.rows[0].email;
-
-    const data6 = await pgClient.query("SELECT * FROM bids WHERE item = 'item6' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item6' ) ORDER BY id");
-    const highBid6 = data6.rows[0].bid;
-    const winningEmail6 = data6.rows[0].email;
-
     return returns.redirect('/');
-    //return returns.render('index', {highBid1:highBid1, winningEmail1:winningEmail1, highBid2:highBid2, winningEmail2:winningEmail2, highBid3:highBid3, winningEmail3:winningEmail3, highBid4:highBid4, winningEmail4:winningEmail4, highBid5:highBid5, winningEmail5:winningEmail5, highBid6:highBid6, winningEmail6:winningEmail6});
+    
 });
 
 // modified from Scaling an Express.js Application with Memcache on Heroku
@@ -194,32 +146,8 @@ app.post('/bid3', async (req, returns) => {
         });
     });
 
-    const data1 = await pgClient.query("SELECT * FROM bids WHERE item = 'item1' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item1' ) ORDER BY id");
-    const highBid1 = data1.rows[0].bid;
-    const winningEmail1 = data1.rows[0].email;
-
-    const data2 = await pgClient.query("SELECT * FROM bids WHERE item = 'item2' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item2' ) ORDER BY id");
-    const highBid2 = data2.rows[0].bid;
-    const winningEmail2 = data2.rows[0].email;
-
-    const data3 = await pgClient.query("SELECT * FROM bids WHERE item = 'item3' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item3' ) ORDER BY id");
-    const highBid3 = data3.rows[0].bid;
-    const winningEmail3 = data3.rows[0].email;
-
-    const data4 = await pgClient.query("SELECT * FROM bids WHERE item = 'item4' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item4' ) ORDER BY id");
-    const highBid4 = data4.rows[0].bid;
-    const winningEmail4 = data4.rows[0].email;
-
-    const data5 = await pgClient.query("SELECT * FROM bids WHERE item = 'item5' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item5' ) ORDER BY id");
-    const highBid5 = data5.rows[0].bid;
-    const winningEmail5 = data5.rows[0].email;
-
-    const data6 = await pgClient.query("SELECT * FROM bids WHERE item = 'item6' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item6' ) ORDER BY id");
-    const highBid6 = data6.rows[0].bid;
-    const winningEmail6 = data6.rows[0].email;
-
     return returns.redirect('/');
-    //return returns.render('index', {highBid1:highBid1, winningEmail1:winningEmail1, highBid2:highBid2, winningEmail2:winningEmail2, highBid3:highBid3, winningEmail3:winningEmail3, highBid4:highBid4, winningEmail4:winningEmail4, highBid5:highBid5, winningEmail5:winningEmail5, highBid6:highBid6, winningEmail6:winningEmail6});
+    
 });
 
 // modified from Scaling an Express.js Application with Memcache on Heroku
@@ -246,32 +174,8 @@ app.post('/bid4', async (req, returns) => {
         });
     });
 
-    const data1 = await pgClient.query("SELECT * FROM bids WHERE item = 'item1' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item1' ) ORDER BY id");
-    const highBid1 = data1.rows[0].bid;
-    const winningEmail1 = data1.rows[0].email;
-
-    const data2 = await pgClient.query("SELECT * FROM bids WHERE item = 'item2' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item2' ) ORDER BY id");
-    const highBid2 = data2.rows[0].bid;
-    const winningEmail2 = data2.rows[0].email;
-
-    const data3 = await pgClient.query("SELECT * FROM bids WHERE item = 'item3' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item3' ) ORDER BY id");
-    const highBid3 = data3.rows[0].bid;
-    const winningEmail3 = data3.rows[0].email;
-
-    const data4 = await pgClient.query("SELECT * FROM bids WHERE item = 'item4' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item4' ) ORDER BY id");
-    const highBid4 = data4.rows[0].bid;
-    const winningEmail4 = data4.rows[0].email;
-
-    const data5 = await pgClient.query("SELECT * FROM bids WHERE item = 'item5' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item5' ) ORDER BY id");
-    const highBid5 = data5.rows[0].bid;
-    const winningEmail5 = data5.rows[0].email;
-
-    const data6 = await pgClient.query("SELECT * FROM bids WHERE item = 'item6' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item6' ) ORDER BY id");
-    const highBid6 = data6.rows[0].bid;
-    const winningEmail6 = data6.rows[0].email;
-
     return returns.redirect('/');
-    //return returns.render('index', {highBid1:highBid1, winningEmail1:winningEmail1, highBid2:highBid2, winningEmail2:winningEmail2, highBid3:highBid3, winningEmail3:winningEmail3, highBid4:highBid4, winningEmail4:winningEmail4, highBid5:highBid5, winningEmail5:winningEmail5, highBid6:highBid6, winningEmail6:winningEmail6});
+    
 });
 
 // modified from Scaling an Express.js Application with Memcache on Heroku
@@ -298,32 +202,8 @@ app.post('/bid5', async (req, returns) => {
         });
     });
 
-    const data1 = await pgClient.query("SELECT * FROM bids WHERE item = 'item1' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item1' ) ORDER BY id");
-    const highBid1 = data1.rows[0].bid;
-    const winningEmail1 = data1.rows[0].email;
-
-    const data2 = await pgClient.query("SELECT * FROM bids WHERE item = 'item2' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item2' ) ORDER BY id");
-    const highBid2 = data2.rows[0].bid;
-    const winningEmail2 = data2.rows[0].email;
-
-    const data3 = await pgClient.query("SELECT * FROM bids WHERE item = 'item3' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item3' ) ORDER BY id");
-    const highBid3 = data3.rows[0].bid;
-    const winningEmail3 = data3.rows[0].email;
-
-    const data4 = await pgClient.query("SELECT * FROM bids WHERE item = 'item4' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item4' ) ORDER BY id");
-    const highBid4 = data4.rows[0].bid;
-    const winningEmail4 = data4.rows[0].email;
-
-    const data5 = await pgClient.query("SELECT * FROM bids WHERE item = 'item5' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item5' ) ORDER BY id");
-    const highBid5 = data5.rows[0].bid;
-    const winningEmail5 = data5.rows[0].email;
-
-    const data6 = await pgClient.query("SELECT * FROM bids WHERE item = 'item6' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item6' ) ORDER BY id");
-    const highBid6 = data6.rows[0].bid;
-    const winningEmail6 = data6.rows[0].email;
-
     return returns.redirect('/');
-    //return returns.render('index', {highBid1:highBid1, winningEmail1:winningEmail1, highBid2:highBid2, winningEmail2:winningEmail2, highBid3:highBid3, winningEmail3:winningEmail3, highBid4:highBid4, winningEmail4:winningEmail4, highBid5:highBid5, winningEmail5:winningEmail5, highBid6:highBid6, winningEmail6:winningEmail6});
+   
 });
 
 // modified from Scaling an Express.js Application with Memcache on Heroku.
@@ -350,32 +230,8 @@ app.post('/bid6', async (req, returns) => {
         });
     });
 
-    const data1 = await pgClient.query("SELECT * FROM bids WHERE item = 'item1' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item1' ) ORDER BY id");
-    const highBid1 = data1.rows[0].bid;
-    const winningEmail1 = data1.rows[0].email;
-
-    const data2 = await pgClient.query("SELECT * FROM bids WHERE item = 'item2' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item2' ) ORDER BY id");
-    const highBid2 = data2.rows[0].bid;
-    const winningEmail2 = data2.rows[0].email;
-
-    const data3 = await pgClient.query("SELECT * FROM bids WHERE item = 'item3' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item3' ) ORDER BY id");
-    const highBid3 = data3.rows[0].bid;
-    const winningEmail3 = data3.rows[0].email;
-
-    const data4 = await pgClient.query("SELECT * FROM bids WHERE item = 'item4' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item4' ) ORDER BY id");
-    const highBid4 = data4.rows[0].bid;
-    const winningEmail4 = data4.rows[0].email;
-
-    const data5 = await pgClient.query("SELECT * FROM bids WHERE item = 'item5' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item5' ) ORDER BY id");
-    const highBid5 = data5.rows[0].bid;
-    const winningEmail5 = data5.rows[0].email;
-
-    const data6 = await pgClient.query("SELECT * FROM bids WHERE item = 'item6' AND bid = (SELECT MAX (bid) FROM bids WHERE item = 'item6' ) ORDER BY id");
-    const highBid6 = data6.rows[0].bid;
-    const winningEmail6 = data6.rows[0].email;
-
     return returns.redirect('/');
-    //return returns.render('index', {highBid1:highBid1, winningEmail1:winningEmail1, highBid2:highBid2, winningEmail2:winningEmail2, highBid3:highBid3, winningEmail3:winningEmail3, highBid4:highBid4, winningEmail4:winningEmail4, highBid5:highBid5, winningEmail5:winningEmail5, highBid6:highBid6, winningEmail6:winningEmail6});
+    
 });
 
     //returns.render ('index');
